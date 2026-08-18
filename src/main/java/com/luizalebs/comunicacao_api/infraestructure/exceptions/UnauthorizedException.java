37de359f548693d7d8731249d09c8da2c7179cd1
Daniel Yassuo <daniel.yassuo.rodrigues@gmail.com>
2026-08-18 16:47:19 -0300

    Desafio 1 , implemantação da documentação com o swagger concluída

---
+++ src/main/java/com/luizalebs/comunicacao_api/infraestructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.luizalebs.comunicacao_api.infraestructure.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+    //excessão personalizada
+    public UnauthorizedException(String message, Throwable throwable){
+        super(message, throwable);
+    }
+
+}
---
+++ src/main/java/com/luizalebs/comunicacao_api/infraestructure/security/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.luizalebs.comunicacao_api.infraestructure.security;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@SecurityScheme(name = SecurityConfig.SECURITY_SCHEME, type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT" , scheme = "bearer")
+public class SecurityConfig {
+
+    // Instâncias de JwtUtil e UserDetailsService injetadas pelo Spring
+    public static final String SECURITY_SCHEME = "bearerAuth";
+    private final JwtUtil jwtUtil;
+
+
+    // Construtor para injeção de dependências de JwtUtil e UserDetailsService
+    @Autowired
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+
+    }
+
+    // Configuração do filtro de segurança
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Cria uma instância do JwtRequestFilter com JwtUtil e UserDetailsService
+
+
+
+        http
+                .csrf(csrf -> csrf.disable()) // Desativa proteção CSRF para APIs REST (não aplicável a APIs que não mantêm estado)
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll().anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Configura a política de sessão como stateless (sem sessão)
+                );// Adiciona o filtro JWT antes do filtro de autenticação padrão
+
+        // Retorna a configuração do filtro de segurança construída
+        return http.build();
+    }
+
+    // Configura o PasswordEncoder para criptografar senhas usando BCrypt
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Retorna uma instância de BCryptPasswordEncoder
+    }
+    // Configura o AuthenticationManager usando AuthenticationConfiguration
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        // Obtém e retorna o AuthenticationManager da configuração de autenticação
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
